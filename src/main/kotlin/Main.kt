c6f06d28c287dc00b73be5d2c3e283e39468b689
TajMahal1990 <130366681+TajMahal1990@users.noreply.github.com>
2023-06-14 08:34:38 +0800

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,8 @@ fun main() {
     println("Приложение меломан")
     var amountSum = 100000
     var skidka = 100 //скидка при покупку от 1001 до 10000 рублей
+    var skidkaOtOneCaus = amountSum - (amountSum / 100) * 5 // 5% скидка 
+    var OnePro = skidkaOtOneCaus / 100 // дополнительная скидка
 
     if (amountSum >= 0 && amountSum <= 1000) {
         println("Скидка не предоставляется")
@@ -12,8 +14,13 @@ fun main() {
         print(" рублей")
     } else if (amountSum >= 10_001) {
         println("Вам предоставлена скидка 5%")
-        print("Сумма покупки вместе со скидкой составит ")
-        print(amountSum - (amountSum / 100) * 5)
+        print("Сумма покупки вместе с 5% скидкой составит ")
+        print("$skidkaOtOneCaus")
+        println(" рублей")
+        print("Дополнительная скидка 1% постоянным покупателям составит $OnePro")
+        println(" рублей")
+        print("Общая сумма со всеми скидками составит " )
+        print(skidkaOtOneCaus - OnePro )
         print(" рублей")
     } else println("Ошибка при указании суммы")
 }
